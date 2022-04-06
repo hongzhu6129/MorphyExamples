0b4e2aeaf04329ade71c814383c1adf2a567d0b7
Hong Zhu <hzhu@brookes.ac.uk>
2022-04-06 09:36:49 +0100

    add missing file

---
+++ FaceRecognition/ImageConfig.java
@@ -0,0 +1,6 @@
+package morphy.examples;
+
+public class ImageConfig {
+    public static String seetaFacePath = "D:\\study\\seetaface\\Detection\\x64\\Debug\\Identification.exe ";
+    public static String imagePath = "D:\\study\\seetaface\\gittest\\picture";
+}
