356aa863f29c2b8f41dfa9ba5eda3f26604d3881
Hong Zhu <hzhu@brookes.ac.uk>
2022-04-07 14:30:56 +0100

    update file

--- FaceRecognition/ImageConfig.java
+++ FaceRecognition/ImageConfig.java
@@ -1,6 +1,12 @@
 package morphy.examples;
 
 public class ImageConfig {
-    public static String seetaFacePath = "D:\\study\\seetaface\\Detection\\x64\\Debug\\Identification.exe ";
-    public static String imagePath = "D:\\study\\seetaface\\gittest\\picture";
+	 public static String seetaFacePath = "C:\\Morphy\\FaceTest\\seetaface";
+	 public static String imagePath = "C:\\Morphy\\FaceTest\\picture";
+	 public static void setSeetaFacePath(String path) {
+		 seetaFacePath = path;
+	 }
+	 public static void setImagePath(String path) {
+		 imagePath = path;
+	 }
 }
